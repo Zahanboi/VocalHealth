49939378e49c75ea07bb673b2e0583f601fb23df
Drishti Kaushik <drishtikaushik1238@gmail.com>
2025-07-08 21:05:56 +0530

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,9 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css'
 import { AuthProvider } from "./context/AuthContext";
 import LandingPage from "./pages/LandingPage";
-import AuthPage from "./pages/AuthPage"
-
+import AuthPage from "./pages/AuthPage";
+import Dashboard from './pages/Dashboard';
+import ExercisesPage from './pages/Exercises';
 export default function App() {
 
   return (
@@ -12,8 +13,17 @@ export default function App() {
      <Router>
       <AuthProvider>
       <Routes>
+
             <Route path='/' element={<LandingPage />} />
             <Route path='/auth' element={<AuthPage />} />
+
+             <Route element={<ProtectedRoute />}>
+
+              <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/exercise" element={<ExercisesPage />} />
+
+             </Route>
+
       </Routes>
       </AuthProvider>
      </Router>
